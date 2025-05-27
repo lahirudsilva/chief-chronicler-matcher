5b932c384362bd925e8cd8c24446927fa35efc2a
Lahiru De Silva <desilvalahiru4@gmail.com>
2025-05-27 20:00:57 +0530

    feat(frontend): configure tRPC client with type safety

---
+++ apps/frontend/src/utils/trpc.ts
@@ -0,0 +1,29 @@
+import { createTRPCNext } from '@trpc/next';
+import { httpBatchLink } from '@trpc/client';
+import type { AppRouter } from '../../../backend/src/router';
+
+function getBaseUrl() {
+  if (typeof window !== 'undefined') return ''; // browser should use relative url
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+  return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
+}
+
+export const trpc = createTRPCNext<AppRouter>({
+  config() {
+    return {
+      links: [
+        httpBatchLink({
+          /**
+           * If you want to use SSR, you need to use the server's full URL
+           * @link https://trpc.io/docs/ssr
+           **/
+          url: `${getBaseUrl()}/api/trpc`,
+        }),
+      ],
+    };
+  },
+  /**
+   * @link https://trpc.io/docs/ssr
+   **/
+  ssr: false,
+});
